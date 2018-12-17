9b99a3b547971a8dd5c8d9dc8aad87f451260069
Laimison <laimison1@gmail.com>
2018-12-17 01:48:53 +0000

    React Slick loaded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,6 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "./index.css";
 
-// react-image-gallery
-// import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
-// import ImageGallery from 'react-image-gallery';
-
 export default class App extends Component {
   render() {
     const settings = {
@@ -16,63 +12,33 @@ export default class App extends Component {
       slidesToShow: 3,
       slidesToScroll: 1
     };
-    
-    const images = [
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      },
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      },
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      },
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      },
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      },
-      {
-        original: 'http://localhost:5000/1000x600.jpeg',
-        thumbnail: 'http://localhost:5000/250x150.jpeg',
-      }
-    ]
+
     return (
       <div>
         <div className="container">
            <Slider {...settings}>
              <div>
                <a href="https://www.google.com/search?q=1&tbm=isch">
-                 <img src="http://placekitten.com/g/400/200" />
+                 <img src="http://localhost:5000/400x200.jpeg" />
                </a>
              </div>
              <div>
                <a href="https://www.google.com/search?q=2&tbm=isch">
-                 <img src="http://placekitten.com/g/400/200" />
+                 <img src="http://localhost:5000/400x200.jpeg" />
                </a>
              </div>
              <div>
                <a href="https://www.google.com/search?q=3&tbm=isch">
-                 <img src="http://placekitten.com/g/400/200" />
+                 <img src="http://localhost:5000/400x200.jpeg" />
                </a>
              </div>
              <div>
                <a href="https://www.google.com/search?q=4&tbm=isch">
-                 <img src="http://placekitten.com/g/400/200" />
+                 <img src="http://localhost:5000/400x200.jpeg" />
                </a>
              </div>
            </Slider>
          </div>
-
-         {/*
-         <ImageGallery items={images} />
-         */}
        </div>
     );
   }
