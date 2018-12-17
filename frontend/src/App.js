3824c59f4cd2fcf06b85ff49508bb73fdde8026e
Laimison <laimison1@gmail.com>
2018-12-17 01:18:48 +0000

    reverted to react-slick slider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 // react-slick
 import React, { Component } from "react";
 import Slider from "react-slick";
-//import "./index.css";
+import "./index.css";
 
 // react-image-gallery
-import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
-import ImageGallery from 'react-image-gallery';
+// import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
+// import ImageGallery from 'react-image-gallery';
 
 export default class App extends Component {
   render() {
@@ -45,33 +45,34 @@ export default class App extends Component {
     ]
     return (
       <div>
-      {/*
-      <div className="container">
-         <Slider {...settings}>
-           <div>
-             <a href="https://www.google.com/search?q=1&tbm=isch">
-               <img src="http://placekitten.com/g/400/200" />
-             </a>
-           </div>
-           <div>
-             <a href="https://www.google.com/search?q=2&tbm=isch">
-               <img src="http://placekitten.com/g/400/200" />
-             </a>
-           </div>
-           <div>
-             <a href="https://www.google.com/search?q=3&tbm=isch">
-               <img src="http://placekitten.com/g/400/200" />
-             </a>
-           </div>
-           <div>
-             <a href="https://www.google.com/search?q=4&tbm=isch">
-               <img src="http://placekitten.com/g/400/200" />
-             </a>
-           </div>
-         </Slider>
-       </div>
-       */}
+        <div className="container">
+           <Slider {...settings}>
+             <div>
+               <a href="https://www.google.com/search?q=1&tbm=isch">
+                 <img src="http://placekitten.com/g/400/200" />
+               </a>
+             </div>
+             <div>
+               <a href="https://www.google.com/search?q=2&tbm=isch">
+                 <img src="http://placekitten.com/g/400/200" />
+               </a>
+             </div>
+             <div>
+               <a href="https://www.google.com/search?q=3&tbm=isch">
+                 <img src="http://placekitten.com/g/400/200" />
+               </a>
+             </div>
+             <div>
+               <a href="https://www.google.com/search?q=4&tbm=isch">
+                 <img src="http://placekitten.com/g/400/200" />
+               </a>
+             </div>
+           </Slider>
+         </div>
+
+         {/*
          <ImageGallery items={images} />
+         */}
        </div>
     );
   }
