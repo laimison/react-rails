4e6cbd63e991167b99ffa4cefb8be2a27a53b92d
Laimison <laimison1@gmail.com>
2018-12-18 00:17:39 +0000

    some success with react-slick sizing for different screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,39 @@ import "./index.css";
 
 export default class App extends Component {
   render() {
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 700,
-      slidesToShow: 3,
-      slidesToScroll: 1
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1500,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     return (
