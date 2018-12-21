1a5b69ddaffe52165b92110bdcbaa1aae894be13
laimison <laimison1@gmail.com>
2018-12-21 22:03:41 +0200

    added scripts to start React and Rails

--- create_app.sh
+++ create_app.sh
@@ -127,4 +127,21 @@ received_signal()
 cd frontend && yarn start || npm start' > up.sh
 chmod +x up.sh
 
+# Script to start React
+echo '#!/bin/bash
+
+yarn start || npm start' > frontend/start.sh
+chmod +x start.sh
+
+# Script to start Rails
+echo '#!/bin/bash
+
+if ! (echo $EMAIL_ADDRESS | grep -q @ && echo $EMAIL_PASSWORD | grep -q '[a-zA-Z0-9]')
+then
+  echo "Warning: You have not specified 'export EMAIL_ADDRESS' and 'export EMAIL_PASSWORD' environment variables for backend mailer so emails will not work"
+  sleep 8
+fi
+
+rails s' > backend/start.sh
+
 echo "Your app created at ${app_dir}"
