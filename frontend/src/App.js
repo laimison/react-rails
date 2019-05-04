74582cbe26ef5f4cb127ee792d549318bde76937
laimison <laimison1@gmail.com>
2019-05-05 00:33:01 +0100

    project adapted to Docker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -53,22 +53,22 @@ export default class App extends Component {
         <Slider {...settings}>
           <div>
             <a href="https://www.google.com/search?q=1&tbm=isch">
-              <img src="http://localhost:5000/400x200.jpeg" />
+              <img src="http://localhost:4000/400x200.jpeg" />
             </a>
           </div>
           <div>
             <a href="https://www.google.com/search?q=2&tbm=isch">
-              <img src="http://localhost:5000/400x200.jpeg" />
+              <img src="http://localhost:4000/400x200.jpeg" />
             </a>
           </div>
           <div>
             <a href="https://www.google.com/search?q=3&tbm=isch">
-              <img src="http://localhost:5000/400x200.jpeg" />
+              <img src="http://localhost:4000/400x200.jpeg" />
             </a>
           </div>
           <div>
             <a href="https://www.google.com/search?q=4&tbm=isch">
-              <img src="http://localhost:5000/400x200.jpeg" />
+              <img src="http://localhost:4000/400x200.jpeg" />
             </a>
           </div>
         </Slider>
